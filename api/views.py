1cf0013c9a7bfa7b3fdfc3a83c01f5d1d737ac02
tanmaydatta <tamnaydatta@gmail.com>
2016-05-26 23:40:13 +0530

    bhot time baad commit

--- api/views.py
+++ api/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+import requests
+from django.template import Context
+from django.http import *
+from django.template.loader import get_template
+from django.views.decorators.csrf import *
+# from auth import *
+from models import *
+import json
 
 # Create your views here.
+def hello(request):
+	return HttpResponse("Hello, world.")
