ad96f6009cc9cea399da6b61890896a666129a8d
tanmaydatta <tamnaydatta@gmail.com>
2016-05-28 11:59:59 +0530

    separate insert city and state

--- init_db/insert.py
+++ init_db/insert.py
@@ -3,31 +3,18 @@ import json
 from api.models import *
 
 
-f=open("init_db/cities.json")
+f=open("states.json")
 x=f.read()
 x=json.loads(x)
-cities = x
-out = []
-for st in cities:
+states = x
+# out = []
+for st in states:
 	try:
-		stobj = StatesUT.objects.get(place_id=st['state_id'])
-		# print stobj
-		try:
-			obj = Cities(name=st['name'],lat=st['_geoloc']['lat'],lng=st['_geoloc']['lng'],place_id=st['place_id'],state_id=stobj.place_id)
-			obj.save()
-			out.append(st)
-		except Exception,e:
-			print str(e)
-			# break
-			pass
+		obj = StatesUT(name=st['name'],lat=st['_geoloc']['lat'],lng=st['_geoloc']['lng'],place_id=st['place_id'])
+		obj.save()
 	except:
-		# print "not found state"
 		pass
-	
 	# print res['results']
 	# break
 
-f.close()
-f=open("init_db/cities.json","w")
-f.write(json.dumps(out))
 f.close()
